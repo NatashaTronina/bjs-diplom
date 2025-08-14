ad962aa911e26b0a2aae43d22f096ec30d2e4db7
Natasha Tronina <tronina.2004@yandex.ru>
2025-08-14 23:19:30 +0500

    виджет профиля отображается

--- public/homePage.js
+++ public/homePage.js
@@ -5,9 +5,18 @@ const logoutUser = new LogoutButton();
 logoutUser.action = () => {
     ApiConnector.logout((response) =>{
         if(response && response.success === true){
-            location.reload()
+            location.reload();
+        }else{
+            userForm.setLoginErrorMessage("Не удалось выйти");
         }
     });
 };
 
+ApiConnector.current((response) => {
+  if (response && response.success === true) { 
+    ProfileWidget.showProfile(response.data); 
+  } else {
+    console.error("Ошибка при получении данных текущего пользователя:", response);
+  }
+});
 
